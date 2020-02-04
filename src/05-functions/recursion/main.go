cd00f575e2d3201b11fac3b836fcbdbb06770ab2
John Hunt <jeh@midmarsh.co.uk>
2020-02-04 19:04:35 +0000

    Further function examples

---
+++ src/05-functions/recursion/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func factorial(i int) int {
+	if i <= 1 {
+		return 1 // Termination condition
+	}
+	return i * factorial(i-1) // Recursive call
+}
+
+func main() {
+	fmt.Println("Starting")
+
+	var i int = 15
+	fmt.Printf("Factorial of %d is %d", i, factorial(i))
+
+	fmt.Println("Done")
+}
