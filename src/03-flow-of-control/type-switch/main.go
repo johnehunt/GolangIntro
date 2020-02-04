e57581b8f0d80bc7a3625987248d47af0127d450
John Hunt <jeh@midmarsh.co.uk>
2020-02-04 10:13:54 +0000

    Added type switch example

---
+++ src/03-flow-of-control/type-switch/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting")
+
+	var x interface{}
+
+	switch x.(type) {
+	case nil:
+		fmt.Println("x is nil")
+	case int:
+		fmt.Println("x is int")
+	case float64:
+		fmt.Println("x is float64")
+	case func(int) float64:
+		fmt.Println("x is func(int)")
+	case bool, string:
+		fmt.Println("x is bool or string")
+	default: /* Optional */
+		fmt.Println("don't know the type")
+	}
+
+	fmt.Println("Done")
+
+}
