88c883b2e881757e9e327be668de2f3ea3e852b6
John Hunt <jeh@midmarsh.co.uk>
2020-03-12 17:53:02 +0000

    goroutine examples

---
+++ src/11-goroutines/basic-goroutine/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func printer(msg string) {
+	for i := 0; i < 10; i++ {
+		fmt.Print(msg)
+	}
+}
+func main() {
+	fmt.Println("Starting")
+
+	printer("A")
+
+	// Invoke function as a goroutine
+	// A goroutine is a lightweight thread of execution
+	go printer("B")
+
+	fmt.Print("X")
+
+	// Can also start a goroutine for an anonymous
+	// function call
+	go func(msg string) {
+		for i := 0; i < 10; i++ {
+			fmt.Print(msg)
+		}
+	}("C")
+
+	fmt.Print("Y")
+
+	// Pause to give goroutines time to complete
+	time.Sleep(time.Second)
+	fmt.Print("Z")
+
+	fmt.Println("\nDone")
+}
---
+++ src/11-goroutines/wait-group/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printer(msg string, wg *sync.WaitGroup) {
+	// On return, notify the WaitGroup that we’re done.
+	defer wg.Done()
+
+	for i := 0; i < 10; i++ {
+		fmt.Print(msg)
+	}
+}
+func main() {
+	fmt.Println("Starting")
+
+	// Waitgroup to wait until a group of goroutines finish
+	fmt.Println("setting up the wait group")
+	var wg sync.WaitGroup
+
+	// Launch several goroutines and
+	// increment the WaitGroup counter for each
+	wg.Add(1)
+	go printer("A", &wg)
+	wg.Add(2)
+	go printer("B", &wg)
+	go printer("C", &wg)
+
+	// Wait for goroutines to complete
+	wg.Wait()
+
+	fmt.Println("\nDone")
+}
