211cd88f5050e1badf084c0827b63733e47844c5
John Hunt <jeh@midmarsh.co.uk>
2020-03-14 16:25:18 +0000

    Extended init(0 function example)

--- src/01-HelloWorld/init-behaviour/main.go
+++ src/01-HelloWorld/init-behaviour/main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 )
 
+var name string
+
 func init() {
 	fmt.Println("Init Method on main initialization")
+	name = "John"
 }
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println("Hello", name)
 }
