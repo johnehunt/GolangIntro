f2d09f8ed87c36edb3732798d7d4969ff6a11b0d
John Hunt <jeh@midmarsh.co.uk>
2020-03-17 17:54:00 +0000

    Reordered output

--- src/02-core-elements/strings/main.go
+++ src/02-core-elements/strings/main.go
@@ -16,19 +16,19 @@ func main() {
 	fmt.Printf("quoted string: ")
 	fmt.Printf("%+q \n", sampleText)
 
-	fmt.Printf("String Length is: ")
-	fmt.Println(len(greeting))
-
 	var s1 = "hello"
 	var s2 = " world"
 	var s3 = s1 + s2
-	fmt.Printf("%s", s3)
+	fmt.Printf("%s\n", s3)
 
 	str := `This is a
 multiline
 string.`
 
 	fmt.Println(str)
+
+	fmt.Print("String Length is: ")
+	fmt.Println(len(greeting))
 
 	// true
 	fmt.Println(strings.Contains("test", "es"))
