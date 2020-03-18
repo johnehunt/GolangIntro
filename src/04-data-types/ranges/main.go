ca88726fc44bac1863475d42c8ce067a2f854d08
John Hunt <jeh@midmarsh.co.uk>
2020-03-18 14:27:42 +0000

    Tidied range examples

--- src/04-data-types/ranges/main.go
+++ src/04-data-types/ranges/main.go
@@ -7,32 +7,17 @@ import (
 func main() {
 	fmt.Println("Starting")
 
-	/* create a slice */
-	numbers := []int{0, 1, 7, 3, 9, 2, 6, 11, 2}
-
-	/* print the numbers */
+	// prints contents of numbers
+	numbers := []int{0, 1, 7, 3}
 	for i, e := range numbers {
-		fmt.Println("Slice item", i, "is", e)
+		fmt.Printf("Item @ %d = %d\n", i, e)
 	}
 
-	/* print elements of a string */
-	for s, c := range "John" {
-		fmt.Printf("s: %v, d %v; ", s, c)
+	// print elements of a string
+	for s := range "John" {
+		fmt.Printf("s: %v; ", s)
 	}
 	fmt.Println()
-
-	/* create a map*/
-	countryCapitalMap := map[string]string{"France": "Paris", "Italy": "Rome", "Japan": "Tokyo"}
-
-	/* print map using keys*/
-	for country := range countryCapitalMap {
-		fmt.Println("Capital of", country, "is", countryCapitalMap[country])
-	}
-
-	/* print map using key-value*/
-	for country, capital := range countryCapitalMap {
-		fmt.Println("Capital of", country, "is", capital)
-	}
 
 	fmt.Println("Done")
 }
