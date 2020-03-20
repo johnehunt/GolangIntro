7888745e538dcda9e7fba5f5bd1cf1b458193150
John Hunt <jeh@midmarsh.co.uk>
2020-03-20 11:21:19 +0000

    Added additional print statements

--- src/12-goroutines/wait-group/main.go
+++ src/12-goroutines/wait-group/main.go
@@ -17,9 +17,10 @@ func main() {
 	fmt.Println("Starting")
 
 	// Waitgroup to wait until a group of goroutines finish
-	fmt.Println("setting up the wait group")
+	fmt.Println("Setting up the wait group")
 	var wg sync.WaitGroup
 
+	fmt.Println("Starting Goroutines")
 	// Launch several goroutines and
 	// increment the WaitGroup counter for each
 	wg.Add(1)
@@ -29,6 +30,7 @@ func main() {
 	go printer("C", &wg)
 
 	// Wait for goroutines to complete
+	fmt.Println("Waiting for Goroutines to finish")
 	wg.Wait()
 
 	fmt.Println("\nDone")
