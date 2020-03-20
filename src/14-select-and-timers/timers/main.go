6fa53e2bcc6d738aacc381f3a8e8a03c689307f2
John Hunt <jeh@midmarsh.co.uk>
2020-03-20 16:46:24 +0000

    Simplified example

--- src/14-select-and-timers/timers/main.go
+++ src/14-select-and-timers/timers/main.go
@@ -10,25 +10,20 @@ func main() {
 	fmt.Println("Starting")
 
 	// Timers represent a single event in the future
-	timer1 := time.NewTimer(2 * time.Second)
+	timer := time.NewTimer(2 * time.Second)
 
-	// blocks on the timer’s channel C until it
-	// sends a value indicating that the timer fired
-	<-timer1.C
-	fmt.Println("Timer 1 fired")
-
-	// Example fo cancelling a timmer before it fires
-	timer2 := time.NewTimer(time.Second)
 	go func() {
-		<-timer2.C
-		fmt.Println("Timer 2 fired")
+		// blocks on the timer’s channel C until it
+		// sends a value indicating that the timer fired
+		<-timer.C
+		fmt.Println("Timer fired")
 	}()
 
-	// Try commenting out the following
-	stop2 := timer2.Stop()
-	if stop2 {
-		fmt.Println("Timer 2 stopped")
-	}
+	// Try uncommenting  the following
+	// stop := timer.Stop()
+	// if stop {
+	// 	fmt.Println("Timer stopped")
+	// }
 
 	time.Sleep(4 * time.Second)
 
