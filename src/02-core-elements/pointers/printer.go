4e42b0728040a7b2be982a20c789a825cb3fd152
John Hunt <john.hunt10@gmail.com>
2021-07-26 14:08:10 +0100

    Added further examples of declaring a pointer

--- src/02-core-elements/pointers/printer.go
+++ src/02-core-elements/pointers/printer.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 
-	var x = 1       // Var containing vaklue 1
+	var x = 1 // Var containing value 1
+
+	var ptr *int = &x // long hand form of declaring type of pointer
+	fmt.Println(*ptr)
+
 	var p = &x      // var containing the address of the variable x
-	fmt.Println(*p) // Print result of dereferencing p
+	fmt.Println(*p) // Print result of dereferencing p - goes to variable referenced by p
+
+	ptr2 := &x // Short hand form of declaring and intiialising a pointer to x
+	fmt.Println(*ptr2)
 
 	*p = 2         // Equal to x = 2
 	fmt.Println(x) // will now print 2
