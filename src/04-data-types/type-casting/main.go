e4c2e7c02619929f4f6f88bc2b687f3606d2426f
John Hunt <john.hunt10@gmail.com>
2021-07-26 19:41:10 +0100

    Extended the type casting examples

--- src/04-data-types/type-casting/main.go
+++ src/04-data-types/type-casting/main.go
@@ -1,6 +1,10 @@
+// Note Go doesn’t support implicit type conversion
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var total int = 120
@@ -8,5 +12,34 @@ func main() {
 	var average float32
 
 	average = float32(total) / float32(count)
-	fmt.Printf("The average was: %0.2f\n", average)
+	fmt.Printf("The average is: %0.2f\n", average)
+
+	i := int(average)
+	fmt.Printf("The integer average is: %d\n", i)
+
+	// Converting a String to an int
+	var s string = "42"
+	v, e := strconv.Atoi(s)
+
+	if e != nil {
+		fmt.Printf("Something when wrong %v\n", e)
+	} else {
+		fmt.Printf("The string as an int is %d\n", v)
+	}
+
+	// Converting an int to a String
+	age := 42
+	str := strconv.Itoa(age) // convert int to string
+	fmt.Printf("The int as a string is: %s\n", str)
+
+	// Converting a string to a float
+	f := "3.14159265"
+	if s, err := strconv.ParseFloat(f, 64); err == nil {
+		fmt.Println(s) // 3.14159265
+	}
+
+	// Convert a float to a string
+	str2 := fmt.Sprintf("%4.2f", 123.456)
+	fmt.Println(str2)
+
 }
