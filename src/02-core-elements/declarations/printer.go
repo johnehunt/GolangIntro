d16b4a72bd6f3dc3bf052df9dbdee52cc590fe37
John Hunt <john.hunt10@gmail.com>
2021-07-26 13:53:25 +0100

    More comments

--- src/02-core-elements/declarations/printer.go
+++ src/02-core-elements/declarations/printer.go
@@ -5,16 +5,22 @@ import (
 	"strconv"
 )
 
-// MAX a max value
+// MAX a max value at top level
 const MAX int = 10
 
 func main() {
+	// short hand form of declaring var of type string
+	// note use of declaration and assignment operator ':='
 	content := ""
+
+	// short hand for of declaring result as variable of typ eint
 	result := 5
 	for i := 1; i < MAX; i++ {
+		// Note use of assignment operator '='
 		content = content + "string" + strconv.Itoa(i) + ", "
 		result = result * i
 	}
+
 	fmt.Println(content)
 	fmt.Printf("Total calculated was = %d", result)
 
