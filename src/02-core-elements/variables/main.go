042e861c0a1836c8840dfb94a493c04a9e6eb617
John Hunt <john.hunt10@gmail.com>
2021-07-26 19:15:15 +0100

    Additional example sof variable assignment

--- src/02-core-elements/variables/main.go
+++ src/02-core-elements/variables/main.go
@@ -6,12 +6,30 @@ import (
 )
 
 func main() {
+
+	var address string
+	address = "10 High Street"
+	fmt.Println(address)
+
 	var i, j int // ints intialized to zero
 	fmt.Printf("Values of i: %d and j: %d\n", i, j)
 
+	var title string = "Dr"
+	var surname = "Cooke"
+	fmt.Printf("%s %s\n", title, surname)
+
 	// Can do multiple declarations and initializations in a line
 	var flag, x, total = true, 23, 42.5
-	fmt.Printf("Values of flag: %t, x: %d and total: %f\n", flag, x, total)
+	fmt.Printf("Values of flag: %t, x: %d and total: %4.2f\n", flag, x, total)
+
+	// Can define set fo different variables and types using var (..)
+	var (
+		name   = "John"
+		age    = 57
+		height int
+	)
+
+	fmt.Printf("%s is %d height: %d\n", name, age, height)
 
 	// Functions can return multiple values
 	// variables can be set based on each
