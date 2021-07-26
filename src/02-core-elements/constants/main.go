e22b44d0fb72716b26aa6f146a048937d0699e9e
John Hunt <john.hunt10@gmail.com>
2021-07-26 13:51:53 +0100

    Additional comments

--- src/02-core-elements/constants/main.go
+++ src/02-core-elements/constants/main.go
@@ -5,16 +5,21 @@ import "fmt"
 func main() {
 	// Good programming practice to define constants in CAPITALS
 
+	// Short hand form where type (string) inferred
 	const TITLE = "Welcome"
 	fmt.Printf("The title is: %s\n", TITLE)
 
+	// Explicit declaration of type for constants
 	const LENGTH int = 10
 	const WIDTH int = 5
+
+	// Declares area variable of type int with 0 as value
 	var area int
 
 	area = LENGTH * WIDTH
 	fmt.Printf("value of area : %d\n", area)
 
+	// Rate with type float
 	const RATE = 3.15
 	fmt.Printf("savings rate is: %f\n", RATE)
 }
