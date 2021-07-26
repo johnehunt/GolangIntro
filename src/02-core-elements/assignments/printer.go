d4ee5765fd01369569097d738c0f44251544a9e9
John Hunt <john.hunt10@gmail.com>
2021-07-26 13:50:06 +0100

    Additional comments

--- src/02-core-elements/assignments/printer.go
+++ src/02-core-elements/assignments/printer.go
@@ -5,22 +5,31 @@ import (
 	"strconv"
 )
 
-// MAX max value
+// MAX max value (not type int is optional)
 const MAX int = 10
 
 func main() {
+	fmt.Println("Starting")
+	// Declare a variable but without a value
+	// Will get the 'zeroeth' value for the type string
+	// which is the empty string ""
 	var content string
+
+	// Declare a variable of type int (inferred) with the value 5
 	var result = 5
 	for i := 1; i < MAX; i++ {
 		content = content + "string" + strconv.Itoa(i) + ", "
 		result = result * i
 	}
 	fmt.Println(content)
-	fmt.Printf("Total calculated was = %d", result)
+	fmt.Printf("Total calculated was = %d\n", result)
 
 	fmt.Println("Assignment Operators")
 
-	var x = 5
+	// Short hand form of declaration and initial vlaue
+	x := 5
+
+	// Various Assignment operators
 	fmt.Printf("x =5: %v\n", x)
 	x += 5
 	fmt.Printf("x +=5: %v\n", x)
@@ -32,5 +41,7 @@ func main() {
 	fmt.Printf("x /=5: %v\n", x)
 	x %= 5
 	fmt.Println("x %=5:", x)
+	x++
+	fmt.Println("x++:", x)
 
 }
