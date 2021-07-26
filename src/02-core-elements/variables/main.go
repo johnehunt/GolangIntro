bddca4572497da605a23fb695b20e2ae93879188
John Hunt <john.hunt10@gmail.com>
2021-07-26 14:04:19 +0100

    Some tidying

--- src/02-core-elements/variables/main.go
+++ src/02-core-elements/variables/main.go
@@ -9,13 +9,16 @@ func main() {
 	var i, j int // ints intialized to zero
 	fmt.Printf("Values of i: %d and j: %d\n", i, j)
 
+	// Can do multiple declarations and initializations in a line
 	var flag, x, total = true, 23, 42.5
-	fmt.Printf("Values of flag: %t, x: %d and total: %f", flag, x, total)
+	fmt.Printf("Values of flag: %t, x: %d and total: %f\n", flag, x, total)
 
 	// Functions can return multiple values
 	// variables can be set based on each
 	var file, err = os.Open("temp.txt")
-	fmt.Println(file)
-	fmt.Println(err)
+
+	// %v is a catch all for printing a value
+	fmt.Printf("file: %v\n", file)
+	fmt.Printf("err: %v\n", err)
 
 }
