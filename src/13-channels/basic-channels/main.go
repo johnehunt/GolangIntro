1b3dacadb5f3549029fc618dcb266dadc7404187
John Hunt <john.hunt10@gmail.com>
2021-07-27 12:02:09 +0100

    Added output to highlight the sequence of steps

--- src/13-channels/basic-channels/main.go
+++ src/13-channels/basic-channels/main.go
@@ -1,21 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
+	fmt.Println("Starting")
 	// Create a new channel that handles strings
 	messageChannel := make(chan string)
 
 	// Use a goroutine to send message
 	go func() {
+		fmt.Println("Go routine - starting to sleep")
+		time.Sleep(2 * time.Second)
+		fmt.Println("Go routine - waking up")
 		// Send a value into a channel using the channel <- syntax
 		messageChannel <- "Hello World"
+		fmt.Println("Go routine - completing")
 	}()
 
+	fmt.Println("Waiting for message")
 	// The <-channel syntax receives a value from the channel
 	msg := <-messageChannel
 
 	// Print out the message received
 	fmt.Println(msg)
+	fmt.Println("Done")
 }
