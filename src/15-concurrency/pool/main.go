c7c53d9ff16797eb6ce6fae69fc13561ecd695dd
John Hunt <john.hunt10@gmail.com>
2021-07-28 14:39:03 +0100

    Added further comments

--- src/15-concurrency/pool/main.go
+++ src/15-concurrency/pool/main.go
@@ -1,27 +1,43 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"sync"
 )
+
+type Info struct {
+	ID   int
+	Data string
+}
+
+func (i *Info) Reset() {
+	i.ID = 0
+	i.Data = ""
+}
 
 func main() {
 	fmt.Println("Starting")
 
 	// Set up Pool
-	var bufferPool = sync.Pool{
+	var infoPool = sync.Pool{
+		// New creates an instance when the pool has nothing available to return.
+		// New must return an interface{} to make it flexible. You have to cast
+		// your type after getting it.
 		New: func() interface{} {
-			return new(bytes.Buffer)
+			return new(Info)
 		},
 	}
 
 	// Obtain an object from the Pool
-	buffer := bufferPool.Get().(*bytes.Buffer)
+	// note need to cast it to a pointer to Info
+	info := infoPool.Get().(*Info)
+	info.ID = 23
+	info.Data = "generla info"
+	fmt.Println(*info)
 
 	// Once finished with it - reset it and return to Pool
-	buffer.Reset()
-	bufferPool.Put(buffer)
+	info.Reset()
+	infoPool.Put(info)
 
 	fmt.Println("Done")
 }
