85a4211fa63c51a28ca8e75ec5cfcd99d76da66f
John Hunt <john.hunt10@gmail.com>
2021-07-30 13:17:15 +0100

    Added a constructor function

--- src/08-embedded-types/multi-embedded-structs/main.go
+++ src/08-embedded-types/multi-embedded-structs/main.go
@@ -21,11 +21,21 @@ type Student struct {
 	Degree
 }
 
+func NewStudent(university string, name string, age int, degreeTitle string, degreeType string) *Student {
+	student := Student{university, Person{name, age}, Degree{degreeTitle, degreeType}}
+	return &student
+}
+
 func main() {
 	var s = Student{"UWE", Person{"John", 21}, Degree{"Computing", "BSc"}}
-	fmt.Println("s:", s)
+	fmt.Printf("s: %v", s)
 
 	fmt.Println("s.Type:", s.Type)
 	// fmt.Println("s.Name:", s.Name) // won't compile ambiguous selector s.Name
 	fmt.Println("s.Person.Name:", s.Person.Name)
+
+	// Can use a constructor function to splify instantiation
+	var s2 = NewStudent("UWE", "Bill", 21, "Cybersecurity", "BSc")
+	fmt.Println(s2)
+
 }
