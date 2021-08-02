e5344926d49404b30e8066cf4660127fecc85143
John Hunt <john.hunt10@gmail.com>
2021-08-02 10:43:26 +0100

    Added a set of constants

--- src/simple-tcp-server/main.go
+++ src/simple-tcp-server/main.go
@@ -5,10 +5,16 @@ import (
 	"simple-tcp-server/server"
 )
 
+const (
+	CONN_HOST = "127.0.0.1"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
 func main() {
 	fmt.Println("Starting Server")
 
-	server.Start("127.0.0.1", "3333", "tcp")
+	server.Start(CONN_HOST, CONN_PORT, CONN_TYPE)
 
 	fmt.Println("Done")
 }
