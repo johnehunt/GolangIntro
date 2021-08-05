2e88b51dd07c8988dae7a0d9d7187934c7bd0370
John Hunt <john.hunt10@gmail.com>
2021-08-05 18:21:19 +0100

    Renamed some variables

--- src/14-further-channels/worker-pools/main.go
+++ src/14-further-channels/worker-pools/main.go
@@ -18,28 +18,28 @@ func main() {
 	fmt.Println("Start")
 
 	// Set up channels
-	const taskNumber = 5
-	tasks := make(chan int, taskNumber)
-	results := make(chan int, taskNumber)
+	const TASK_NUMBER = 5
+	tasksChannel := make(chan int, TASK_NUMBER)
+	resultsChannel := make(chan int, TASK_NUMBER)
 
 	// Start up 3 worker goroutines
 	for w := 1; w <= 3; w++ {
-		go worker(w, tasks, results)
+		go worker(w, tasksChannel, resultsChannel)
 	}
 
 	// Send 5 tasks (numbered 1 to 5) to the
 	// task channel to be processed by the workers
-	for j := 1; j <= taskNumber; j++ {
-		tasks <- j
+	for j := 1; j <= TASK_NUMBER; j++ {
+		tasksChannel <- j
 	}
 
 	// Close the task channel to indicate
 	// that there is no further work to do
-	close(tasks)
+	close(tasksChannel)
 
 	// Collect the results of the work
-	for a := 1; a <= taskNumber; a++ {
-		var r = <-results
+	for a := 1; a <= TASK_NUMBER; a++ {
+		var r = <-resultsChannel
 		fmt.Println("result:", r)
 	}
 
