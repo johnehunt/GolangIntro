4b5f3815608388a00e25a387091219716b5555b0
John Hunt <john.hunt10@gmail.com>
2021-08-12 11:23:43 +0100

    Updates example

--- src/bank-account-channel-examples/channels-accounts/main.go
+++ src/bank-account-channel-examples/channels-accounts/main.go
@@ -11,61 +11,75 @@ all handled via a central monitor which ensures sequential
 access once requests are sent via the channel (which can run concurrently)
 */
 
-// Set up channels for comms
-var depositChannel = make(chan int)
-var withdrawChannel = make(chan int)
-var balanceChannel = make(chan int)
-
-func Deposit(amount int) {
-	depositChannel <- amount
+// Set up bank account and channels for comms
+type Account struct {
+	balance         float64
+	depositChannel  chan float64
+	withdrawChannel chan float64
+	balanceChannel  chan float64
 }
 
-func Withdraw(amount int) {
-	withdrawChannel <- amount
+func NewAccount(initialBalance float64) *Account {
+	acc := Account{
+		balance:         initialBalance,
+		depositChannel:  make(chan float64),
+		withdrawChannel: make(chan float64),
+		balanceChannel:  make(chan float64),
+	}
+	return &acc
 }
 
-func Balance() int {
-	return <-balanceChannel
+func (acc *Account) Deposit(amount float64) {
+	acc.depositChannel <- amount
 }
 
-func monitor() {
-	// balance is lexically confined to function
-	var balance int
+func (acc *Account) Withdraw(amount float64) {
+	acc.withdrawChannel <- amount
+}
+
+func (acc *Account) Balance() float64 {
+	return <-acc.balanceChannel
+}
+
+func (acc *Account) monitor() {
 	for {
 		select {
-		case amount := <-depositChannel:
-			balance = balance + amount
-		case amount := <-withdrawChannel:
-			balance = balance - amount
-		case balanceChannel <- balance:
+		case amount := <-acc.depositChannel:
+			acc.balance = acc.balance + amount
+		case amount := <-acc.withdrawChannel:
+			acc.balance = acc.balance - amount
+		case acc.balanceChannel <- acc.balance:
 		}
 	}
 }
 
-func init() {
-	go monitor() // start the balance monitor goroutine
-}
-
 func main() {
+
 	fmt.Println("Starting")
+	// Create the account
+	acc := NewAccount(0.0)
 
+	// Start the monitor running in the background
+	go acc.monitor()
+
+	fmt.Println("Starting the go routines")
 	go func() {
-		Deposit(200)
-		fmt.Printf("Balance after 200 deposit is %d\n", Balance())
+		acc.Deposit(200)
+		fmt.Printf("Balance after 200 deposit is %4.2f\n", acc.Balance())
 	}()
 
 	go func() {
-		Withdraw(150)
-		fmt.Printf("Balance after 150 withdrawal is %d\n", Balance())
+		acc.Withdraw(150)
+		fmt.Printf("Balance after 150 withdrawal is %4.2f\n", acc.Balance())
 	}()
 
 	go func() {
-		Deposit(100)
-		fmt.Printf("Balance after 100 deposit is %d\n", Balance())
+		acc.Deposit(100)
+		fmt.Printf("Balance after 100 deposit is %4.2f\n", acc.Balance())
 	}()
 
 	time.Sleep(2 * time.Second)
-	fmt.Printf("Final balance is %d\n", Balance())
+	fmt.Printf("Final balance is %4.2f\n", acc.Balance())
 
 	fmt.Println("Done")
 }
@@ -75,10 +89,11 @@ Sample Output:
 --------------
 
 Starting
-Balance after 200 deposit is 200
-Balance after 150 withdrawal is 50
-Balance after 100 deposit is 150
-Final balance is 150
+Starting the go routines
+Balance after 200 deposit is 300.00
+Balance after 150 withdrawal is 150.00
+Balance after 100 deposit is 100.00
+Final balance is 150.00
 Done
 
 */
